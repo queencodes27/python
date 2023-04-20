a1bd34d32c64275076c6ed89d8d914166c279722
nsaip <nadiasaip@gmail.com>
2023-04-20 08:05:23 -0500

    code edited

---
+++ uchinchibosqich.py
@@ -0,0 +1,65 @@
+import math
+#2-misol
+# c = int(input(' C ga qiymat bering: '))
+# x = int(input('X ga qiymat bering: '))
+# t = int(input( 'T ga qiymat bering: '))
+# L = pow(math.tan(c),(-2)) + (2 * pow(x,2) + 5) / math.sqrt(c + t)
+# print(L)
+
+#3-misol
+# y = int(input(' Y ga qiymat bering: '))
+# h = int(input('H ga qiymat bering: '))
+# a = (math.tan(pow(y,3) - pow(h,4)) + h ** 2) / ( math.sin(h) ** 3 + y )
+# print(a)
+
+#5-misol
+# y = int(input(' Y ga qiymat bering: '))
+# x = int(input('X ga qiymat bering: '))
+# z = int(input('Z ga qiymat bering: '))
+# c = int(input('C ga qiymat bering: '))
+
+# A = math.tan(pow(x,4) - 6) - math.cos(z + x * y) ** 3
+# B = math.cos(pow(x,3) * pow(c,2)) ** 4
+# G = A / B
+# print(G)
+
+# 6-misol
+# y = int(input(' Y ga qiymat bering: '))
+# x = int(input('X ga qiymat bering: '))
+# t = int(input('T ga qiymat bering: '))
+ 
+# A = math.sin(x) ** 3 + math.log(2 * y + 3 * x)
+# B = pow(t,math.exp(1)) + math.sqrt(x)
+
+# P = A / B
+# print(P)
+
+#7- misol
+# y = int(input(' Y ga qiymat bering: '))
+# x = int(input('X ga qiymat bering: '))
+# a = int(input('A ga qiymat bering: '))
+# b = int(input('B ga qiymat bering: '))
+
+# A = math.sqrt(x + y - a) + math.log(y)
+# B = math.atan(b + a)
+# T = A / B
+# print(T)
+
+#8-misol
+# y = int(input(' Y ga qiymat bering: '))
+# t = int(input('T ga qiymat bering: '))
+
+# S = (4.351 * pow(y,3) + 2 * t * math.log(t)) / math.sqrt(math.cos(2 * y) + 4.351)
+
+# print(S)
+
+#10-misol
+# y = int(input(' Y ga qiymat bering: '))
+# x = int(input('X ga qiymat bering: '))
+# a = int(input('A ga qiymat bering: '))
+# b = int(input('B ga qiymat bering: '))
+# c = int(input('C ga qiymat bering: '))
+
+# U  = (pow(math.tan(y),3) + math.sin(x * math.sqrt(b - c)) ** 5) / math.sqrt(a - b + c)
+
+# print(U)
