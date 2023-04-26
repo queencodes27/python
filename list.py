4af160a812806b7beaf99955f3432acaf9db581b
nsaip <nadiasaip@gmail.com>
2023-04-26 15:08:02 -0500

    strings

---
+++ list.py
@@ -0,0 +1,15 @@
+""" PYTHON LIST """
+
+# n = [] # shu kvadratniy skobka list degani
+# x = ' '  # bu string chiqoradi
+
+n = [ 12, 9999,-2.5, 0, 1, 78, 98,1]
+m = [ "isksksks" , 1, 3, 6, 8 ]
+# n.append(7) # it adds character to the list
+# n.insert (2, "hwkhw") # THIS remove character in index 2 and inputs given string
+# n.remove(True) # removes the character from the list 
+#n.extend(m) # it adds list from m to n and extends the list
+print (n)  
+
+# n.sort() # sorts from lower to bigger
+# n.sort(reverse=True) # it reverses
---
+++ strmethods.py
@@ -0,0 +1,31 @@
+"""PYTHON STRING ARGUMENTS """
+
+# x = "      WYEHEHE Hello 3PythonW                      "
+# print (x[4]
+# print (x[6:-3])  # Pyt so it prints from backwards
+# print (x[5:1]) 
+
+# print (len(x)) # counts the lenght of the characters 
+# print (x[:]) # prints everything without any chnages 
+# print (x[1:10:2]) # prints every 2'nd character  from "1"st index until "10"th index 
+# print (x[3:15:4]) # prints every 4'th character  from "3'rd index until "15"th index  
+#print (x[3:15-4])
+# print (x.upper()) # prints all characters in upper case
+# print (x.lower()) # prints all characters in lower case
+#print (x.capitalize()) # CAPITALIZES FIRST CHARACTER ONLY AND lowers all other characters
+
+# print (x.title()) # capitalizes first letter in every word but ignores numbers
+# print (x.upper().lower() .capitalize() .title())
+# print (x.find("o")) # prints the index of the letter
+# print (len(x)) 
+#print (x.strip()) # ikkita chettagi probelni tozalap beradi
+# print (x.strip()) # left side'tagi probeli tozalap beradi
+x = "      WYEHEHE Hello 3PythonW                "
+# print (x.rstrip (),  end= ('') )  # lstrip -clear empty space of left side and method "end" stops it 
+# print (x.center (90,  '$')) # 90'ta probel tashap u probellarni orniga '$' qoyip chiqadi va sting'i ortaga yozadi
+
+# print (x.replace ('P', '1')) # replaces letters with given character. FAQAT STRING'DA ISHLIYDI
+# print (x.split ('o')) # probel boyicha bo'ladi va 'o' dan boshlap o'gacha bogan sozlani chiqoradi 
+# print (x.isprintable())  # boolen type method va buni Javobi True or False bolishi mumkun
+
+
