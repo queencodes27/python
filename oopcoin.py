3ec5bcacd88b2586ffd3d4cc54b44504e91e568c
nsaip <nadiasaip@gmail.com>
2023-06-16 15:57:36 -0500

    another class created

---
+++ oopcoin.py
@@ -0,0 +1,69 @@
+class Coin:
+    def __init__(self, value:int) -> None:
+        self.value = value
+    
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+coin1 =Coin(10) 
+coin2 = Coin(11)
+coin3 = Coin(12)
+coins = [coin1, coin2, coin3]
+
+# print(coins)
+    
+class PaperMoney:
+    def __init__(self, value: int) -> None:
+        self.value = value
+    
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+papermoney1 = PaperMoney(20)
+papermoney2 = PaperMoney(30)
+# papermoneys = [papermoney1, papermoney2]
+
+# print(papermoneys)
+
+class Wallet:
+    def __init__(self) -> None:
+        self.all_money = []
+        
+    def getAllPaperMoney(self):
+        s_loop = 0
+        total_money = 0
+        for paper in self.all_money:
+            if type(paper).__name__ == 'PaperMoney':
+                total_money += int(paper.value)
+                s_loop +=1
+        return f" {s_loop}  {total_money}"
+        
+  
+    def getAllCoin(self):
+        s_loop = 0
+        total_money = 0
+        result = ''
+        for paper in self.all_money:
+           if isinstance(paper, PaperMoney):
+               total_money += int(paper.value)
+               s_loop +=1
+               result += f" {s_loop}  {total_money}"
+        return result
+    
+    def getSumMoney(self):
+        result = 0
+        for paper in self.all_money:
+            result += paper.value 
+        return result
+                
+    def addMoney(self, d: PaperMoney|Coin):
+        self.all_money.append(d)
+                
+a = Wallet()
+a.addMoney(papermoney1)
+a.addMoney(papermoney2) 
+a.addMoney(coin1)
+a.addMoney(coin2)
+a.addMoney(coin3)
+print(a.getAllPaperMoney())
+print(a.getSumMoney())
--- oophmw.py
+++ oophmw.py
@@ -113,77 +113,188 @@ ENCAPSULATION
     
     
 
-class Passenger:
-    def __init__(self, passportID: str, fullname: str, gender:str) -> None:
-        self.passportID = passportID
-        self.fullname = fullname
-        self.gender = gender
+# class Passenger:
+#     def __init__(self, passportID: str, fullname: str, gender:str) -> None:
+#         self.passportID = passportID
+#         self.fullname = fullname
+#         self.gender = gender
     
-    def getFull_Info(self):
-        return self.getFull_Info 
+#     def getFull_Info(self):
+#         return f"Passport id:{self.passportID} Full name:{self.fullname} Gender:{self.gender}"
     
-    def __str__(self) -> str:
-        return f"{self.passportID} {self.fullname} {self.gender}"
+#     def __str__(self) -> str:
+#         return self.fullname
 
-passenger = Passenger("31195886", "Elon Musk", "male" )
-print(passenger)
-print()
+# passenger1 = Passenger("31195886", "Elon Musk", "male" )
+# passenger2 = Passenger("81818109", "Bill Gates", "male")
+# passenger3 = Passenger("9098708090", "Sardor M", "male")
+# passengers = [passenger1, passenger2, passenger3]
+# # print(passengers)
 
-class Driver:
-    def __init__(self, passportID: str, fullName: str, age: int) -> None:
-        self.passportID = passportID
-        self.fullName = fullName
-        self.age = age
+# class Driver:
+#     def __init__(self, passportID: str, fullName: str, age: int) -> None:
+#         self.passportID = passportID
+#         self.fullName = fullName
+#         self.age = age
         
-    def getFull_Info(self):
-        return self.getFull_Info   
+#     def getFull_Info(self):
+#         return self.fullName
     
-    def __str__(self) -> str:
-        return f"{self.passportID} {self.fullName} {self.age}"
+#     def __str__(self) -> str:
+#         return f"{self.passportID} {self.fullName} {self.age}"
     
-driver = Driver("AC777555", "Nadira Saip", 18)
-print(driver)
-print()
+# driver = Driver("AC777555", "Nadira Saip", 18)
+# print(driver)
+# print()
 
-class Train:
-    def __init__(self, trainID: str, name: str, speed: int, driver: Driver) -> None:
-        self.trainID = trainID
+# class Train:
+#     def __init__(self, trainID: str, name: str, speed: int, driver: Driver) -> None:
+#         self.trainID = trainID
+#         self.name = name
+#         self.speed = speed
+#         self.driver_obj = driver 
+        
+#     def setName(self):
+#         self.name
+    
+#     def getFull_Info(self):
+#         return f"{self.name}" 
+    
+#     def __str__(self) -> str:
+#         return f" TRAIN_ID: {self.trainID}\n NAME: {self.name}\n SPEED: {self.speed}mph\n DRIVER: {self.driver_obj.getFull_Info()}\n\t "
+    
+# train = Train( "CTA-CHICAGO", "CTA-BlueLine", 70, driver ) 
+# # print(train)  
+
+# class Platform:
+#     def __init__(self, platformID:str, status: bool) -> None:
+#         self.platformID = platformID
+#         self.status = status
+    
+#     def getFull_Info(self):
+#         return f"{self.platformID} {self.status}" 
+    
+#     def __str__(self) -> str:
+#         return f"{self.platformID} {self.status}"
+    
+# platform = Platform("CTA-1", True)
+# print(platform)
+
+# class Trip:
+#     def __init__(self, start_from: str, to: str, train_obj:Train, platform_obj: Platform, passengers:list , priceTrip:int, dateTrip:str) -> None:
+#         self.start_from = start_from
+#         self.to = to
+#         self.train_obj = train_obj
+#         self.platform_obj = platform_obj
+#         self.passengers_obj = passengers
+#         self.priceTrip = priceTrip
+#         self.dateTrip = dateTrip
+     
+#     def showData(self):
+#         return self.train
+    
+#     def getFull_Info(self):
+#         x = []
+#         for i in self.passengers_obj:
+#             x.append(str(i.getFull_Info())+" ")
+#         y = ''.join(x)
+#         return f" FROM:{self.start_from}\nEND:{self.to}\nTRAIN:{self.train_obj}\nPLATFORM:{self.platform_obj}\n PASSENGER:{y}\n PRICE:${self.priceTrip} DATE:{self.dateTrip}"
+    
+#     def toString(self):
+#         return self.train_obj
+        
+#     # def addPassengers(self, passengers_obj: Passenger):
+#     #     self.passengers.append(passengers_obj)
+    
+#     def __str__(self) -> str:
+#         x = []
+#         for i in self.passengers_obj:
+#             x.append(str(i.getFull_Info())+" ")
+#         y = ''.join(x)
+#         return f" FROM: {self.start_from}\n END: {self.to}\n TRAIN: {self.train_obj}\n PLATFORM: {self.platform_obj} PASSENGERS: {y}\n PRICE: ${self.priceTrip} DATE: {self.dateTrip}"
+
+# trip1 = Trip("Uzbekistan", "Switzerland", train, platform,[passenger1,passenger2, passenger3], 0,  "10/01/2023 - 11/01/2023" )
+# trip2 = Trip("Switzerland", "Italy", train, platform,[passenger1, passenger2, passenger3], 0,  "11/01/2023 - 12/01/2023" )
+# trip3 = Trip("Italy", "Greece", train, platform, [passenger1, passenger2, passenger3], 0, "12/01/2023 - 01/01/2024")
+# trips = [trip1, trip2, trip3]
+# # print(trip1.getFull_Info())
+
+
+# class RailwayStation:
+#     def __init__(self,name:str, address: str, trip: list) -> None:
+#         self.name = name
+#         self.address = address
+#         self.trip = trip
+        
+#     def getFull_Info(self):
+#         x = []
+#         for i in self.trip:
+#             x.append(str(i.getFull_Info())+" ")
+#         z = ''.join(x) 
+#         return f" NAME: {self.name}\n ADDRESS: {self.address}\n TRIP: {z}\n"
+    
+    
+#     def __str__(self) -> str:
+#         x = []
+#         for i in self.trip:
+#             x.append(str(i.getFull_Info())+" ")
+#         z = ''.join(x) 
+#         return f" NAME: {self.name}\n ADDRESS: {self.address}\n TRIP: {z}\n"
+    
+# railway = RailwayStation("Subway-USA", "5555 N Cumberland, Chicago, IL 60656", [trip1, trip2, trip3])
+
+# print(railway) 
+
+
+class Author:
+    def __init__(self, first_name:str, last_name:str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def getFullname(self):
+        return self.first_name + ' ' + self.last_name
+    
+    def __str__(self) -> None:
+        return f"{self.first_name} {self.last_name}"
+
+authors1 = Author("Michelle" ,"Obama")
+authors2 = Author("Melinda" ,"Gates")
+authors3 = Author("Maye", "Musk")
+print(authors1.getFullname())
+
+class Books:
+    def __init__(self, name:str, pages:int, authors:Author) -> None:
         self.name = name
-        self.speed = speed
-        self.driver_obj = driver 
+        self.pages = pages
+        self.authors = []
+    
+    def getAuthors(self):
+        x = []
+        for i in self.authors:
+            x.append(str(i.getAuthors()+ " "))
+        y = " ".join(x)
+        return f"  NAME: {self.name}\n PAGES: {self.pages}\n AUTHOR: {y}\n"
+    
+    def getName(self):
+        return self.name
+    
+    def __str__(self) -> str:
+        x = []
+        for i in self.authors:
+            x.append(str(i.getAuthors()+ " "))
+        y = " ".join(x)
         
-    def setName(self):
-        self.name
-    
-    def getFull_Info(self):
-        return self.getFull_Info 
-    
-    def __str__(self) -> str:
-        return f" TRAIN_ID: {self.trainID}\n NAME: {self.name}\n SPEED: {self.speed}mph\n DRIVER: {self.driver_obj}\n\t "
-    
-train = Train( "CTA-CHICAGO", "CTA-BlueLine", 70, driver ) 
-print(train)  
+        return f" NAME: {self.name}\n PAGES: {self.pages}\n AUTHOR: {y}\n"        
+book1 = Books("Becoming Michelle Obama",448, "Michelle Obama")
+book2 = Books("Moment of Lift", 288, "Melinda Gates")
+book3 = Books("A Woman Makes a Plan", 224, "Maye Musk")
+x = [book1, book2, book3]
+print(book1.getName)
 
-class Platform:
-    def __init__(self, platformID:str, status: bool) -> None:
-        self.platformID = platformID
-        self.status = status
-    
-    def getFull_Info(self):
-        return self.getFull_Info   
-    
-    def __str__(self) -> str:
-        return f"{self.platformID} {self.status}"
-    
-platform = Platform("CTA-1", True)
-print(platform)
+            
+        
 
-class Trip:
-    def __init__(self, start_from: str, to: str, train:Train, platform: Platform, passengers: Passenger, priceTrip:int, dateTrip:str) -> None:
-        self.start_from = start_from
-        self.to = to
-        self.train_obj = train
-        self.platform_obj = platform
-        self.passenger_obj = []
-        self.
+
+        
+    
     
