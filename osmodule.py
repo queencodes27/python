1dc3d071b461a8b44d1a0d3bf65483ad100a6a3e
queencodes27 <saipnadira@gmail.com>
2023-06-19 08:57:31 -0500

    os module edited

--- osmodule.py
+++ osmodule.py
@@ -1,10 +1,25 @@
-Python OS module provides easy functions that allow us to interact and
-get Operating System information and even control processes up to a limit.
+# Python OS module provides easy functions that allow us to interact and
+# get Operating System information and even control processes up to a limit.
+# It is a standard library within Python 3 installation. You just need to import it to use. 
 
 import os
-from datetime 
-print(os.getcwd()) : prints current working dir
-print(os.listdir()) : prints  list of dir 
-os.mkdir
-os.stat : this prints out information about the file such as st.mtime, st.
-os.walk()
+from pathlib import Path
+
+
+# from datetime 
+# print(os.getcwd()) : prints current working dir
+# print(os.listdir()) : prints  list of dir 
+# os.mkdir
+# os.stat : this prints out information about the file such as st.mtime, st.size=20 : string size in the file
+# os.walk()
+
+os.path.basename(path())
+print(os.basename(path))
+
+uname : 
+fchown: Change the owner and group id of the file given by fd to the numeric uid and gid. 
+fsync: force write files with filedescriptor fd to disk. 
+pipe : create a pipe. return a pair of file descriptors (r, w) usable for reading and writing
+
+
+
