28174c9afb97fc33b45820ec39619b99a2c4cc35
queencodes27 <saipnadira@gmail.com>
2023-06-19 07:40:18 -0500

    Os Module file created

---
+++ osmodule.py
@@ -0,0 +1,10 @@
+Python OS module provides easy functions that allow us to interact and
+get Operating System information and even control processes up to a limit.
+
+import os
+from datetime 
+print(os.getcwd()) : prints current working dir
+print(os.listdir()) : prints  list of dir 
+os.mkdir
+os.stat : this prints out information about the file such as st.mtime, st.
+os.walk()
