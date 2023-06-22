462c5ff505f189671d9c6aeecd770a9d4646baf8
queencodes27 <saipnadira@gmail.com>
2023-06-22 06:15:49 -0500

    edited

--- race.py
+++ race.py
@@ -1,6 +1,16 @@
 import time
-
+import os
+a = " "
+b = " "
+c = " "
 emojis = '\U0001f3ce' 
 start = time.time
 while True:
+    os.system("clear")
+    a += " " 
+    b += "  "
+    c += "   "
+    time.sleep(0.3)
+    print(f"")
+    
     
