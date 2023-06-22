cb1fb2e4165efdc3376376b72f340b08f6203de8
queencodes27 <saipnadira@gmail.com>
2023-06-22 08:25:58 -0500

    code added

--- race.py
+++ race.py
@@ -1,16 +1,25 @@
 import time
 import os
+import random
 a = " "
 b = " "
 c = " "
 emojis = '\U0001f3ce' 
 start = time.time
+counter = 0
 while True:
     os.system("clear")
-    a += " " 
-    b += " "
-    c += "   "
+    a += " "* random.randint(0,5)
+    b += " " * random.randint(0,5)
+    c += " " * random.randint(0,5)
     # time.sleep(1)
     print(f"{b} {emojis}", '\n', f'{a} {emojis}', "\n", f"{c} {emojis}" )
+    counter += 1
+    if counter == 30:
+        break 
     time.sleep(0.3)
+    
+    
+    
+    
     
