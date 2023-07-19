f167b5451605f2abdd0c0160dc601e34dceeb5bf
queencodes27 <saipnadira@gmail.com>
2023-07-19 16:47:12 +0500

    test

--- txt.py
+++ txt.py
@@ -1,8 +1,16 @@
-m = "Python" 
-print(m)
-# s = m[::-1]
-# print(s)
-n = []
-print(chr(112),chr(89),chr(84),chr(72),chr(79), chr(7))
+# m = "Python" 
+# print(m)
+# # s = m[::-1]
+# # print(s)
+# n = []
+# print(chr(112),chr(89),chr(84),chr(72),chr(79), chr(7))
 
+import math
+# print(math.sqrt(-1)) 
 
+# x = math.log(d) +( 3.5 * d ** 2 ) + 1 / math.cos(2* y)
+# l = math.sqrt(math.exp(x))- math.cos(pow(x,2))* pow(a,5) ** 4 + math.atan(a - pow(x, 5)**4) / math.exp(1) * math.sqrt(abs(a + x * pow(c,4)))
+
+x = (A or not A and B ) or C
+print(x)
+A, B, C = True, False, False
